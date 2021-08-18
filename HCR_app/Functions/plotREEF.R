b8375dc8cb32b689e04789f07a4a37ff4cd3c4b8
jerellejesse <jjesse@neptune.gmri.org>
2021-08-18 14:52:41 -0400

    + REE plots

---
+++ HCR_app/Functions/plotREEF.R
@@ -0,0 +1,17 @@
+plotREEF <- function(om,rho,freq)
+{
+  df<-read.csv(here('Data/shiny_data_jj.csv'))
+  df<-df[df$OM==om,]
+  df<-df[df$Rho==rho,]
+  df<-df[df$Frequency==freq,]
+  df$HCR[df$HCR==1]<-'Ramp'
+  df$HCR[df$HCR==2]<-'Feco'
+  df$HCR[df$HCR==3]<-'F-step'
+  df$HCR[df$HCR==4]<-'Constrained ramp'
+  df$HCR<-as.factor(df$HCR)
+  df<-df[df$Year>2019,]
+  ggplot(df)+geom_line(aes(x=Year,y=REEF,color=HCR))+
+    theme_classic()+theme(text=element_text(size=18),legend.position='none')+
+    ylab('% REE F')+
+    scale_color_colorblind()
+}
